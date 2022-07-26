33127284b4be8f4cba3acbde9fb41a74ee37a3a8
KPAMsar <kpamsarshija@gmail.com>
2022-07-26 22:11:22 +0100

    initial cleanup

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\pages;
+use App\Http\Controllers\pagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +14,12 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/index',[pagesController::class,'index'])->name('index');
+ Route::get('login',[pagesController::class,'login'])->name('login');
+ Route::get('register',[pagesController::class,'register'])->name('register');
+ Route::get('forgot-password',[pagesController::class,'forgotPassword'])->name('forgot_password');
